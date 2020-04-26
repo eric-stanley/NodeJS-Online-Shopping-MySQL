afd3eff22df91e3cc7344f72bdad4b5741797c5c
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2020-04-26 23:05:55 +0530

    Added ejs view engine

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(favicon(__dirname + '/favicon.ico'));
@@ -21,6 +21,7 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
  res.status(404).render('404', {
   title: 'Page not found',
+  path: '/'
  });
 });
 
